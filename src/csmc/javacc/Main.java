56b80bcc38acecb0885f6e74ee8ff3e65b013542
Timofey Lyssenko <tim.lyssenko@gmail.com>
2017-03-02 17:08:55 +0300

    Added SimpleVisitor that traverses the AST, added comprehensive Test2.cs

--- src/csmc/javacc/Main.java
+++ src/csmc/javacc/Main.java
@@ -6,16 +6,19 @@ package csmc.javacc;
 
 import csmc.javacc.generated.CSharpParser;
 import csmc.javacc.generated.ParseException;
+import csmc.javacc.generated.SimpleNode;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws ParseException, FileNotFoundException {
-        InputStream is = new ByteArrayInputStream("(".getBytes());
         CSharpParser parser = new CSharpParser(new FileInputStream("tests/Test1.cs"));
-        parser.Input();
+
+        SimpleNode parseTree = parser.Input();
+//        parseTree.dump("");
+
+        SimpleVisitor sv = new SimpleVisitor();
+        parseTree.jjtAccept(sv, null);
     }
 }
